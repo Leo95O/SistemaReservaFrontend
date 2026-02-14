3e94039d6caba20f024d462a41c7ef0e0994a460
Leonardo <joelolaya681@gmail.com>
2026-02-14 10:15:32 -0500

    Actualización de enviroment

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   // Esta es la dirección por defecto de NestJS
-  apiUrl: 'http://localhost:3000' 
+  apiUrl: 'http://localhost:3000/api/v1'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   // Cuando subas el backend a la nube (AWS, DigitalOcean, etc.),
   // pondrás aquí la URL real (ej: 'https://api.misisistema.com').
   // Por ahora, déjalo igual o pon una cadena vacía para que no falle el build.
-  apiUrl: 'http://localhost:3000' 
+  apiUrl: 'http://localhost:3000/api/v1'
 };
